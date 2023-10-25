3aa40d8b94f1711641c82ede7e780d45a64237bc
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-10-25 23:32:03 +0600

    hometask lesson 2.2

--- src/week1/day2/Homework.java
+++ src/week1/day2/Homework.java
@@ -13,6 +13,34 @@ public class Homework {
         // System.out.println("System.out.print(\"C\\\\:Program Files\\\\VS2005\\n\");");
 
         // Task #4 solution
-        // System.out.println();
+        // System.out.println("\\t\tprints a tab");
+        // System.out.println("\\\"\tprints a double quotation character (\")");
+        // System.out.println("\\\\\tprints a backslash character (\\)");
+        // System.out.println("\\n\tprints a new line character");
+        // System.out.println("\\b\tinserts a backspace");
+
+        // task #5
+        // System.out.println("public class MyFirstCode {");
+        // System.out.println("\tpublic static void main(String []args) {");
+        // System.out.println("\t\tSystem.out.print(\"Hello world\\n\");");
+        // System.out.println("\t\tSystem.out.print(\"My first \\\"JAVA\\\" program\");");
+        // System.out.println("\t}");
+        // System.out.println("}");
+
+        // task #6
+        // System.out.println("+----------------------------------------------------+");
+        // System.out.println("|                     Times of day                   |");
+        // System.out.println("+----------------+------------------+----------------+");
+        // System.out.println("|     Morning    |    Afternoon     |     Evening    |");
+        // System.out.println("+----------------+------------------+------------ ---+");
     }
 }
+
+
+
+
+
+
+
+
+
