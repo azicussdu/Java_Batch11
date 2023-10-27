391342564b9759ad2f36e6bb55fa674d3fa58b75
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-10-27 23:21:53 +0600

    class code

--- src/week1/day4/Homework.java
+++ src/week1/day4/Homework.java
@@ -19,6 +19,18 @@ public class Homework {
 //        double massIndex = weight / (height * height);
 //        System.out.println("Mass index is " + massIndex);
 
+        // Task #3.
+//        int num1 = 4;
+//        int num2 = 5;
+//
+//        double average = (double) (num1 + num2) / 2;
+//        System.out.println("The average is " + average);
+
+        // Task #4.
+//        int num = 42;
+//        double fivePercent = (double)num * 5 / 100;
+//        System.out.println("5% is " + fivePercent);
+
 
     }
 }
