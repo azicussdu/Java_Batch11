81be49effd14c284b521bd41e0a21b8e31517375
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-10-28 01:51:00 +0600

    class code

---
+++ src/week1/day5/Homework.java
@@ -0,0 +1,73 @@
+package week1.day5;
+
+import java.util.Scanner;
+
+public class Homework {
+    public static void main(String[] args) {
+
+        // Task #1. Average of two integers
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("enter first integer: ");
+//        int num1 = scan.nextInt();
+//
+//        System.out.print("enter second integer: ");
+//        int num2 = scan.nextInt();
+//
+//        double average = (num1 + num2) / (double) 2;
+//        System.out.println("The average is " + average);
+
+        // Task #2. Area and perimeter of a circle.
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("enter a radius: ");
+//        double radius = scan.nextDouble();
+//
+//        double perimeter = 2 * 3.14 * radius;
+//        double area = 3.14 * radius * radius;
+//
+//        System.out.println("The perimeter equals to " + perimeter);
+//        System.out.println("The area equals to " + area);
+
+        // Task #3. Divide first into second number
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("enter two integers: ");
+//        int num1 = scan.nextInt();
+//        int num2 = scan.nextInt();
+//
+//        double result = (double) num1 / num2;
+//        System.out.println("The result of division is " + result);
+
+        //Task #4. Mass index.
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("enter your height(in meters): ");
+//        double height = scan.nextDouble();
+//
+//        System.out.print("enter your weight(in kilos): ");
+//        double weight = scan.nextDouble();
+//
+//        double massIndex = weight / (height * height);
+//        System.out.println("Your mass index is " + massIndex);
+
+        //Task #5.
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("enter amount of days: ");
+//        int days = scan.nextInt();
+//
+//        int fullWeeks = days / 7;
+//        System.out.println("Amount if full weeks is " + fullWeeks);
+
+        //Task #6.
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("enter amount of seconds: ");
+//        int seconds = scan.nextInt();
+//
+//        int fulMinutes = seconds / 60;
+//        System.out.println("Amount if full minutes is " + fulMinutes);
+
+    }
+}
---
+++ src/week1/day5/TestScanner.java
@@ -0,0 +1,24 @@
+package week1.day5;
+
+import java.util.Scanner; // it appears when you click "import class" on Scanner word
+
+public class TestScanner {
+    public static void main(String[] args) {
+        /*
+        Scanner is something we use to read some data from somewhere.
+        System.in - means that I will read data from keyboard
+        scan - just a name for my Scanner thing (you can give any name)
+         */
+//        Scanner scan = new Scanner(System.in);
+
+//        System.out.print("Please enter any integer: ");
+//        int num = scan.nextInt(); //scan.nextInt() - read an integer from keyboard
+//        System.out.println("you entered number: " + num);
+
+//        System.out.print("enter a double number: ");
+//
+//        double num = scan.nextDouble();
+//        System.out.println("You entered " + num);
+
+    }
+}
