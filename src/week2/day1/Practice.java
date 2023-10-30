ed61923b986a2f6046589704eee2fe7df09f57bd
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-10-31 00:41:09 +0600

    class code

---
+++ src/week2/day1/Practice.java
@@ -0,0 +1,17 @@
+package week2.day1;
+
+public class Practice {
+    public static void main(String[] args) {
+
+        /*
+        The system ask a user to enter fullname (surname and name).
+        After the input, the system have
+        to show the first letter surname and first letter of name
+
+        enter a fullname: Max Holloway
+        first letter of name is: M
+        first letter of surname is: H
+         */
+
+    }
+}
---
+++ src/week2/day1/TestString1.java
@@ -0,0 +1,65 @@
+package week2.day1;
+
+import java.util.Scanner;
+
+public class TestString1 {
+    public static void main(String[] args) {
+
+//        String myname = "Aliye";
+//        System.out.println("my name is " + myname);
+
+//        String sent = "I am learning Java now";
+//        System.out.println("the sentence is " + sent);
+
+        // -------------- using Scanner ------------------
+
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("Enter your name: ");
+//        //scan.next() - reads only ONE word from keyboard
+//        String name = scan.next();
+//
+//        System.out.println("you entered " + name);
+
+
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("Enter your surname and name: ");
+//        // scan.nextLine() - reads before it meets ENTER (reads the whole line)
+//        String fullname = scan.nextLine();
+//
+//        System.out.println("you entered " + fullname);
+
+        // -------------- length() ---------------
+
+//        String city = "New York";
+//
+//        int cityLength = city.length();
+//        System.out.println("The length is " + cityLength + " letters");
+
+//        System.out.println("the length is " + city.length());
+
+        // -------------- charAt() ---------------
+
+//        String country = "Puerto Rico";
+//
+//        char letter = country.charAt(7);
+//        System.out.println("The letter at position 7 is " + letter);
+
+//        System.out.println(country.charAt(10));
+
+        // ---------------- indexOf() ------------------
+
+//        String sentence = "Java is the best";
+
+//        System.out.println("the position of v is " + sentence.indexOf("v"));
+//        System.out.println("the position of t is " + sentence.indexOf("t"));
+//
+//        System.out.println("the position of word the is " + sentence.indexOf("the"));
+
+//        int firstT = sentence.indexOf("t"); // firstT = 8
+//        System.out.println("the first t position is " + firstT);
+//        System.out.println("the second t position is " + sentence.indexOf("t", firstT+1));
+
+    }
+}
