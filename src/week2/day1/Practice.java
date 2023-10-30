0a9d9819589353270e1686c875449826a9787d18
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-10-31 01:49:48 +0600

    class code

--- src/week2/day1/Practice.java
+++ src/week2/day1/Practice.java
@@ -1,5 +1,7 @@
 package week2.day1;
 
+import java.util.Scanner;
+
 public class Practice {
     public static void main(String[] args) {
 
@@ -12,6 +14,33 @@ public class Practice {
         first letter of name is: M
         first letter of surname is: H
          */
+
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("Enter a fullname: ");
+//        String fullname = scan.nextLine();
+//
+//        System.out.println("1st letter of 1st word is " + fullname.charAt(0));
+//
+//        int spacePos = fullname.indexOf(" ");
+//        System.out.println("1st letter of 2nd word is " + fullname.charAt(spacePos + 1));
+
+
+        //The same task, but find last letters of each word
+
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("Enter a fullname: ");
+//        String fullname = scan.nextLine();
+//
+//        int spacePos = fullname.indexOf(" ");
+//        int length = fullname.length();
+//
+//        System.out.println("last letter of 1st word is " + fullname.charAt(spacePos-1));
+//
+//        System.out.println("last letter of 2nd word is " + fullname.charAt(length-1));
+
+
 
     }
 }
