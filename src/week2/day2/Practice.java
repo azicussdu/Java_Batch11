5fb6974ce7d2fea81d5db7f6ad282be8827d690d
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-10-31 19:50:17 +0600

    class code

---
+++ src/week2/day2/Practice.java
@@ -0,0 +1,61 @@
+package week2.day2;
+
+import java.util.Scanner;
+
+public class Practice {
+    public static void main(String[] args) {
+
+        /*
+        user inputs 2 word. The program have to show
+        each word separately.
+
+        Enter 2 words: Java language
+
+        1st word is: Java
+        2nd word is: language
+         */
+
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Enter 2 words: ");
+//
+//        String sent = scan.nextLine();
+//        int spacePos = sent.indexOf(" ");
+//
+//        String word1 = sent.substring(0, spacePos);
+//        String word2 = sent.substring(spacePos+1);
+//
+//        System.out.println("the first word is: " + word1);
+//        System.out.println("the second word is: " + word2);
+
+
+        /*
+        User enters a word. Program must change
+        only the first letter to an uppercase letter.
+
+        Enter a word: python
+        After change: Python
+         */
+
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("enter a word: ");
+//        String word = scan.next(); // word = "python"
+//
+//        String result = word.substring(0, 1).toUpperCase()
+//                + word.substring(1);
+//
+//        System.out.println("result is: " + result);
+
+        /*
+
+        User inputs 3 words, the program should show only 1 and 3 word.
+
+        Enter 3 words: my name anna
+
+        First word is: My
+        Third word is: annA
+
+         */
+
+    }
+}
---
+++ src/week2/day2/TestString2.java
@@ -0,0 +1,114 @@
+package week2.day2;
+
+import java.util.Scanner;
+
+public class TestString2 {
+    public static void main(String[] args) {
+
+        // ------------- indexOf vs lastIndexOf --------------------
+
+//        String city = "Rio Do Janeiro";
+
+//        System.out.println("first o's position is " + city.indexOf("o"));
+//        System.out.println("second o's position is " + city.indexOf("o", 3));
+
+//        System.out.println("last o's position is " + city.lastIndexOf("o"));
+//        System.out.println("previous o's position is " + city.lastIndexOf("o", 12));
+
+        // -------------------- contains() -------------------
+
+//        String sentence = "Hello from Java lessons";
+//        boolean hasWord = sentence.contains("Java");
+//
+//        System.out.println("it has Java word is " + hasWord);
+
+
+//        String sentence = "My name is Max";
+//        boolean hasWord = sentence.contains("Anna");
+//
+//        System.out.println("it contains Anna is " + hasWord);
+
+
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("Enter a sentence: ");
+//        String sentence = scan.nextLine();
+//
+//        System.out.print("Enter a word: ");
+//        String word = scan.next();
+//
+//        boolean hasWord = sentence.contains(word);
+//        System.out.println("it contains your word is " + hasWord);
+
+        // ---------------- concat ----------------------
+
+//        String s1 = "hello";
+//        String s2 = "world";
+//
+//        String s3 = s1 + s2;
+//        System.out.println("s3 is " + s3);
+//
+//        String s4 = "hello" + " " + "world";
+//        System.out.println("s4 is " + s4);
+
+
+//        String s1 = "hello";
+//        String s2 = "world";
+//
+//        String s3 = s1.concat(s2);
+//        System.out.println("s3 is " + s3);
+//
+//        String s4 = s1.concat(" ").concat(s2);
+//        System.out.println("s4 is " + s4);
+
+        // ----------- equals and equalsIgnoreCase ------------
+
+//        String s1 = "Java";
+//        String s2 = "java";
+//
+//        boolean areEqual = s1.equals(s2);
+//        System.out.println("the result is " + areEqual);
+
+
+//        String s1 = "JAVA";
+//        String s2 = "java";
+//
+//        boolean areEqual = s1.equalsIgnoreCase(s2);
+//        System.out.println("the result is " + areEqual);
+
+        // -------------- isEmpty -----------------
+
+//        String name = "";
+//        System.out.println(name.isEmpty());
+
+        // ------------- toLowerCase and toUpperCase ------------
+
+//        String city = "London";
+//        String cityChanged = city.toUpperCase();
+//        System.out.println("changed city is " + cityChanged);
+
+
+//        String city = "MANCHESTER";
+//        String cityChanged = city.toLowerCase();
+//        System.out.println("changed city is " + cityChanged);
+
+        // ------------ substring -------------
+
+//        String sent = "Topic is Java";
+//        String word = sent.substring(0, 5);
+//        System.out.println("word is " + word);
+
+//        String sent = "Topic is Java";
+//        String sbs = sent.substring(6);
+//        System.out.println("sbs: " + sbs);
+    }
+}
+
+
+
+
+
+
+
+
+
