bb9aac3ff9ca55ce254f6fc45734d3c7a67dcd7d
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-11-14 02:49:33 +0600

    class code

---
+++ src/week4/day1/Arrays_3.java
@@ -0,0 +1,48 @@
+package week4.day1;
+
+import java.util.Scanner;
+
+public class Arrays_3 {
+    public static void main(String[] args) {
+
+        /*
+        enter size of array: 5
+        java will fill array with random number from 1 to 10
+        show all numbers from array
+
+        count the odd and even numbers and print:
+        there are 2 odd numbers
+        and 3 even numbers
+         */
+
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("enter size of array: ");
+//        int size = scan.nextInt();
+//
+//        int[] numbers = new int[size];
+//
+//        for(int i=0; i<size; i++){
+//            numbers[i] = (int)(Math.random() * 10 + 1); // 1 - 10
+//        }
+//
+//        System.out.print("numbers are: ");
+//        for(int i=0; i<size; i++){
+//            System.out.print(numbers[i] + " ");
+//        }
+//
+//        int countOdds = 0;
+//        int countEvens = 0;
+//
+//        for(int i=0; i<size; i++){
+//            if(numbers[i] % 2 == 0)
+//                countEvens++;
+//            else
+//                countOdds++;
+//        }
+//
+//        System.out.println("\namount of evens = " + countEvens);
+//        System.out.println("amount of odds = " + countOdds);
+
+    }
+}
---
+++ src/week4/day1/Practice.java
@@ -0,0 +1,22 @@
+package week4.day1;
+
+public class Practice {
+    public static void main(String[] args) {
+
+        /*
+        enter the size: 5
+        enter values: 40 54 26 76 87
+
+        87 76 26 54 40
+         */
+
+
+        /*
+        enter the size: 5
+        enter values: 40 -54 26 -76 87
+
+        Positive
+         */
+
+    }
+}
