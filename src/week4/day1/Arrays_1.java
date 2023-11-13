e0e73feb37de74a88ec556dfc1e21290bc98921c
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-11-14 02:02:33 +0600

    class code

---
+++ src/week4/day1/Arrays_1.java
@@ -0,0 +1,34 @@
+package week4.day1;
+
+import java.util.Scanner;
+
+public class Arrays_1 {
+    public static void main(String[] args) {
+
+//        Scanner scan = new Scanner(System.in);
+//        int[] marks = new int[10]; // this is how we create an array of size 10
+//
+//        System.out.print("enter 10 marks: ");
+//
+//        for(int i=0; i<10; i++){
+//            marks[i] = scan.nextInt();
+//        }
+//
+//        System.out.println("Now will show marks >= 50: ");
+//
+//        for(int i=0; i<10; i++){
+//            if(marks[i] >= 50)
+//                System.out.println(marks[i]);
+//        }
+
+//        System.out.println("first mark = " + marks[0]);
+//        System.out.println("last mark = " + marks[9]);
+
+//        System.out.println("Now will show all values in my array: ");
+//
+//        for(int i=0; i<10; i++){
+//            System.out.println(marks[i]);
+//        }
+
+    }
+}
---
+++ src/week4/day1/Arrays_2.java
@@ -0,0 +1,33 @@
+package week4.day1;
+
+import java.util.Scanner;
+
+public class Arrays_2 {
+    public static void main(String[] args) {
+
+        /*
+        enter size for your array: 5
+        Now enter values for your array: 34 12 54 65 76
+
+        The average of values = 46.65
+         */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("enter size for your array: ");
+        int size = scan.nextInt();
+
+        int[] numbers = new int[size];
+
+        System.out.print("Now enter values for your array: ");
+        for(int i=0; i<size; i++){
+            numbers[i] = scan.nextInt();
+        }
+
+        int total = 0;
+        for(int i=0; i<size; i++){
+            total = total + numbers[i];
+        }
+
+        System.out.println("The average of all numbers = " + ((double)total / size));
+    }
+}
