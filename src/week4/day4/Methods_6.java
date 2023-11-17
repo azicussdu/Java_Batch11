c75bec1ced093131739c25946e948c9c9110ff5e
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-11-18 00:25:54 +0600

    class code

--- src/week4/day4/Methods_6.java
+++ src/week4/day4/Methods_6.java
@@ -1,10 +1,12 @@
 package week4.day4;
 
+import java.util.Arrays;
+
 public class Methods_6 {
 
     public static void fillWithRandom(int[] numbers){
         for(int i=0; i<numbers.length; i++){
-            numbers[i] = (int)(Math.random() * 10 + 1); // 1-10
+            numbers[i] = (int)(Math.random() * 100 + 1); // 1-10
         }
     }
 
@@ -20,7 +22,33 @@ public class Methods_6 {
         fillWithRandom(numbers);
         printArray(numbers);
 
+        System.out.println();
+
+        showMaxNumber2(numbers);
+
 //        showMaxNumber(numbers); // show the maximum value inside array
-//        showMinValue(numbers); // show the minimum value inside array
+//        showMinNumber(numbers); // show the minimum value inside array
+    }
+
+    public static void showMaxNumber2(int[] numbers){
+        int max = numbers[0];
+
+        for(int i=0; i<numbers.length; i++){
+            if(numbers[i] > max){
+                max = numbers[i];
+            }
+        }
+
+        System.out.println("max = " + max);
+    }
+
+    public static void showMaxNumber(int[] numbers){
+        Arrays.sort(numbers);
+        System.out.println("max is = " + numbers[numbers.length-1]);
+    }
+
+    public static void showMinNumber(int[] numbers){
+        Arrays.sort(numbers);
+        System.out.println("min is = " + numbers[0]);
     }
 }
