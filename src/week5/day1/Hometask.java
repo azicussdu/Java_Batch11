899f36680a3a3fee63a603f8d22f21453262a07e
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-11-21 02:49:53 +0600

    class code

---
+++ src/week5/day1/Hometask.java
@@ -0,0 +1,39 @@
+package week5.day1;
+
+public class Hometask {
+    public static void main(String[] args) {
+
+        /*
+        User will enter several names until string "exit"
+
+        Program has to ask to enter two names:
+            first name is to search for
+            second name is to replace with
+
+        If program finds inside names arraylist the first name
+        then program will replace it with the second name.
+
+        If program can not find first name inside arraylist, then
+        it just adds second name in the beginning of array
+         */
+
+        /*
+        Hanna Sahin Polina Hamza exit
+
+        first name: Sahin
+        second name: Aliye
+
+        [Hanna Aliye Polina Hamza] // so it replace the Sahin with Aliye
+         */
+
+        /*
+        Hanna Sahin Polina Hamza exit
+
+        first name: Tetiana
+        second name: Aliye
+
+        [Aliye Hanna Sahin Polina Hamza] // so just Aliye was added in the beginning
+         */
+
+    }
+}
