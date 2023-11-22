b0d9ee3d84aa5f6e230615cd8c4ac356af1055c1
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-11-23 02:33:57 +0600

    class code

--- src/week5/day3/Practice.java
+++ src/week5/day3/Practice.java
@@ -1,20 +1,42 @@
 package week5.day3;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class Practice {
     public static void main(String[] args) {
+
+//        HashSet<Integer> hs = new HashSet<>(Arrays.asList(10, 20, 30, 40, 50));
+//        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(40, 50));
+
+        // from hs remove all elements that you can find in list
+//        hs.removeAll(list);
+        // before hs = [10, 20, 30, 40, 50]
+        // after hs = [10, 20, 30]
+
+        // -----------------------------------------------------------------
 
         ArrayList<String> engGrp = new ArrayList<>(Arrays.asList("Hamza", "Aliye", "Oguzhan", "Khalid"));
         ArrayList<String> proGrp = new ArrayList<>(Arrays.asList("Khalid", "Aliye", "Polina", "Hanna"));
 
         /*
+        show students who study only english
+        output: Hamza, Oguzhan
+         */
+
+
+
+        /*
         Show all student names (without duplicating them).
         Show in alphabetic order.
 
-        Aliye, Hamza, Hanna, Khalid, Oguzhan, Polina
+        Should be an output like this: Aliye, Hamza, Hanna, Khalid, Oguzhan, Polina
          */
+
+//        TreeSet<String> ts = new TreeSet<>();
+//        ts.addAll(engGrp);
+//        ts.addAll(proGrp);
+
+//        System.out.println("ts values: " + ts);
 
     }
 }
