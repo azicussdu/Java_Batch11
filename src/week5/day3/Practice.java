114413a80df0071825f7b6b4be21430405a2d9fd
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-11-23 02:09:14 +0600

    class code

---
+++ src/week5/day3/Practice.java
@@ -0,0 +1,20 @@
+package week5.day3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Practice {
+    public static void main(String[] args) {
+
+        ArrayList<String> engGrp = new ArrayList<>(Arrays.asList("Hamza", "Aliye", "Oguzhan", "Khalid"));
+        ArrayList<String> proGrp = new ArrayList<>(Arrays.asList("Khalid", "Aliye", "Polina", "Hanna"));
+
+        /*
+        Show all student names (without duplicating them).
+        Show in alphabetic order.
+
+        Aliye, Hamza, Hanna, Khalid, Oguzhan, Polina
+         */
+
+    }
+}
