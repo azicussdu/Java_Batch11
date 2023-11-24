eb91f155f354e5386b503874bcf4604eadad84ab
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-11-25 03:17:24 +0600

    class code

---
+++ src/week5/day5/Hometask.java
@@ -0,0 +1,30 @@
+package week5.day5;
+
+public class Hometask {
+    public static void main(String[] args) {
+
+        /*
+        Just need to do some modification
+        in our project from Map_dictionary.java file.
+
+        - For menu #2, If he enters the same translation that we had
+        in our dictionary then print the message:
+        "The new translation is the same as old one".
+        After he normally has to choose menu #2 again and
+        again try to enter a new translation.
+
+        - For menu #3. If there are no words added yet into dictionary
+        then just print message "Dictionary is empty".
+        You can do it using isEmpty() or size() methods of TreeMap class.
+
+        - For menu #4. If the word entered does not exist in our
+        dictionary so ask the user if he wants to add it?
+        if user inputs "yes" then ask for its translation and after
+        add it to the dictionary. If user inputs "no", then ignore.
+
+        - For menu #5. If he wants to remove a word that does not
+        exist in our dictionary then print message "No such word in a dictionary".
+         */
+
+    }
+}
