1b9bd444f9e8a4584ab8a279d8e314f9b4ea3f99
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-11-29 01:50:24 +0600

    class code

--- src/week6/day2/ex3/Student.java
+++ src/week6/day2/ex3/Student.java
@@ -5,7 +5,28 @@ public class Student {
     int age;
     double gpa; // min:0 - max:4  (2.8)
 
-    public static void showStudentInfo(){
-        System.out.println();
+    public void showStudentInfo(){
+        System.out.println("name: " + name);
+        System.out.println("age: " + age);
+        System.out.println("gpa: " + gpa);
+    }
+
+    public String getStudentInfo(){
+        String info = "name is " + name + "\n";
+        info = info + "age is " + age + "\n";
+        info = info + "gpa is " + gpa + "\n";
+
+        // String info = "name is Mary\nage is 19\ngpa is 3.4\n";
+        return info;
+    }
+
+    // if you call it "toString()" - so it is a magic method
+    public String toString(){ // should be String (not void) also name = toString()
+        String info = "name is " + name + "\n";
+        info = info + "age is " + age + "\n";
+        info = info + "gpa is " + gpa + "\n";
+
+        // String info = "name is Mary\nage is 19\ngpa is 3.4\n";
+        return info;
     }
 }
--- src/week6/day2/ex3/TestStudent.java
+++ src/week6/day2/ex3/TestStudent.java
@@ -8,13 +8,32 @@ public class TestStudent {
         stud1.age = 19;
         stud1.gpa = 3.4;
 
+        Student stud2 = new Student();
+        stud2.name = "Mike";
+        stud2.age = 20;
+        stud2.gpa = 3.2;
+
         // first way
-        System.out.println("name: " + stud1.name);
-        System.out.println("age: " + stud1.age);
-        System.out.println("gpa: " + stud1.gpa);
+//        System.out.println("name: " + stud1.name);
+//        System.out.println("age: " + stud1.age);
+//        System.out.println("gpa: " + stud1.gpa);
 
         // second way
+//        stud1.showStudentInfo();
+//        stud2.showStudentInfo();
 
+        // third way
+//        String stud1Info = stud1.getStudentInfo();
+//        System.out.println(stud1Info);
+//
+//        System.out.println(stud2.getStudentInfo());
+
+        // forth way
+        System.out.println(stud1.toString());
+
+        // even if you don't call toString() method
+        // java will automatically find it and call
+        System.out.println(stud1);
 
     }
 }
