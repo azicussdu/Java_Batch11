cb603f74fd6f1bd95dc14233bc3d1e9258279cf7
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-11-29 02:27:15 +0600

    class code

---
+++ src/week6/day2/Hometask.java
@@ -0,0 +1,14 @@
+package week6.day2;
+
+public class Hometask {
+    /*
+    Create a class Circle that has a field: double radius;
+
+    Also inside a class create next methods:
+        - showPerimeter() // will print the perimeter
+        - showArea() // will print the area
+        - getPerimeter() // will return perimeter
+        - getArea() // will return area
+        - toString() // will return all information about the circle object
+     */
+}
