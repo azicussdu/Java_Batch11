2032b2a25493aa94316540cc351b3951b0ab0ceb
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-11-30 02:48:48 +0600

    class code

---
+++ src/week6/day3/Hometask.java
@@ -0,0 +1,20 @@
+package week6.day3;
+
+public class Hometask {
+    /*
+
+    create a class Lesson:
+        fields: String name; int credits;  // math (5 credits), english(4 credits)
+
+    create a class Student:
+        fields: String name; ArrayList<Lesson> lessonsList = ...
+        methods:
+            showAllLessons() - prints all lessons for an individual student object
+            showStudentRules() - prints the list of rules for students in university
+            double getBusySemesters() - in 1 semester student can study only 30 credits
+
+            assume that student "Mike" has 75 credits - it means that he will
+            study 2.5 semesters (75/30 = 2.5)
+
+     */
+}
