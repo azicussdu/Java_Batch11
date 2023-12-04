25341c3ba5a075e570cb9363066d17abd48a04fc
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-12-05 01:34:39 +0600

    class code

---
+++ src/week7/day1/ex4/PracticeTask.java
@@ -0,0 +1,27 @@
+package week7.day1.ex4;
+
+public class PracticeTask {
+
+    /*
+    Create a Person:
+        fields: String name, boolean male
+        create 2 constuctors: a) name and male  b) male
+
+    In a TestPerson java file:
+        create several objects of a Person class.
+        some objects as male and as female.
+
+        You need to count amount male objects, female objects and total objects.
+
+    Person p1 = new Person("Mary", false);
+    Person p2 = new Person(true);
+    Person p3 = new Person("Mike", true);
+    Person p4 = new Person(true);
+
+    // 3 male objects
+    // 1 female objects
+    // 4 objects total
+
+     */
+
+}
