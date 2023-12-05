76f258d331d980c3efc4d70c3630990c6bde5239
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-12-06 02:02:51 +0600

    class code

---
+++ src/week7/day2/ex3/Task.java
@@ -0,0 +1,19 @@
+package week7.day2.ex3;
+
+public class Task {
+
+    /*
+    enum Role: ADMIN, MODERATOR, USER
+
+    enum Status: ONLINE, OFFLINE, BANNED
+
+    create a class Account:
+        fields: name, role and status
+
+    in TestAccount: create ArrayList of Accounts and test it
+
+    create static method showAccountsByRoleAndStatus(usersList, role, status)
+
+     */
+
+}
