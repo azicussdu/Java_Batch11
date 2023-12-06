c0bdf7051c3b3fc88d0520dda3adc6cd4f0767d3
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-12-07 02:48:55 +0600

    class code

--- src/week7/day3/Hometask.java
+++ src/week7/day3/Hometask.java
@@ -3,9 +3,18 @@ package week7.day3;
 public class Hometask {
 
     /*
-    Task   Worker(arraylist tasks)
+    class Food:
+        String name; (if you want also can validate)
+        int price (only positive number);
 
+    class Menu:
+        String restaurantName;
+        ArrayList<Food> foodList = ...
 
+    For those two class provide getter/setter, constructor(at least 1 constructor),
+    toString(). Do data validation in setter methods. Test it after.
+
+    !!! Hanna - Do not use GENERATE MENU :))) This week try to do it manually!!!
      */
 
 }
