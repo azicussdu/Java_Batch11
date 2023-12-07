7b794cbca428b1a381ab66219748f02d9188e2a3
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-12-08 02:50:11 +0600

    class code

---
+++ src/week7/day4/HomeTask.java
@@ -0,0 +1,30 @@
+package week7.day4;
+
+public class HomeTask {
+
+    /*
+
+    create a class Animal:
+        fields: String name, boolean isWild
+        methods:
+            void makeSound(){ sout("making some sound") }
+
+    create a class Dog: that inherits from Animal:
+        fields: String breed;
+        methods:
+            Override makeSound(){ sound of dog }
+
+    create a class Cat: that inherits from Animal:
+        fields: boolean isLazy
+        methods:
+            Override makeSound(){ sound of cat }
+
+    create a class Tiger: inherits from a Cat:
+        fields: think of some field here
+        methods:
+            Override makeSound(){ sound of tiger }
+
+
+     */
+
+}
