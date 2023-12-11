428a80841faed97923f566d0e3379560aeebe7e5
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-12-12 01:44:00 +0600

    class code

---
+++ src/week8/day1/ex1/Task.java
@@ -0,0 +1,29 @@
+package week8.day1.ex1;
+
+public class Task {
+    /*
+    class Shape:
+        no fields.
+        methods:
+            showPerimeter() - sout("some perimeter for a shape")
+            showArea() - sout("some area for a shape")
+
+    class Rectangle(inherits from Shape):
+        fields: int width, int length
+        methods: Override methods from parent class
+        create constructor
+        create toString() - should show perimeter and area
+
+   class Square(inherits from Rectangle):
+        fields: no fields (just those which it inherits from Rectangle)
+        methods: Think, should you override methods or just leave as it is
+        create constructor
+        create toString() - should show perimeter and area
+
+   class Circle(inherits from Shape):
+        fields: double radius
+        methods: Override methods from Shape
+        create constructor
+        create toString() - should show perimeter and area
+     */
+}
