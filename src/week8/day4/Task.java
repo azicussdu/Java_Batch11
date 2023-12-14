bd6211ad103658b9563815bf86941a3ecd211bcb
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-12-15 02:50:12 +0600

    class code

---
+++ src/week8/day4/Task.java
@@ -0,0 +1,44 @@
+package week8.day4;
+
+public class Task {
+    /*
+
+    enum Universe {DC, MARVEL}
+
+    abstract class Superhero
+        fields: Universe universe;
+        provide constructor(with universe)
+        create abstract method attack();
+
+    interface IFlyable
+        void attackFromSky();
+
+    interface IShootable
+        void shoot();
+
+    ---- all next classes should extend from Superhero ----
+    class Superman is superhero that can fly and shoot
+    class Rocket is superhero that can only shoot
+    class Batman is superhero that can only fly
+    class Groot is superhero
+    class Ironman is superhero that can fly and shoot
+
+    in main method create arraylist of all heros
+
+    then try test.
+
+
+     */
+
+    public static void main(String[] args) {
+        // all heros in arraylist
+    }
+
+    public static void skyAttack(ArrayList<Superhero> herosList){
+        //only those who can fly should attack from sky
+    }
+
+    public static void attackWithShoot(ArrayList<Superhero> herosList){
+        //only those who can shoot should shoot
+    }
+}
