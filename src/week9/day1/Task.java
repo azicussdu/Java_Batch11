d685e47832506e5597f64ddd2167e4aab5657fd4
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-12-19 02:29:23 +0600

    class code

---
+++ src/week9/day1/Task.java
@@ -0,0 +1,23 @@
+package week9.day1;
+
+public class Task {
+
+    /*
+    Change the Superhero example.
+
+    Create a class Team:
+        fields: - ArrayList<Superhero> herosList = ...
+
+        void addSuperhero(Superhero hero){...}
+        // this method just adds an object of hero to a arraylist of heros
+
+        // add the names to all your superheros
+        void removeSuperhero(String heroName){...}
+
+        //create next non-static
+        void attackFromSky(){...} - all heros from arrayList that can fly should attack from sky
+
+        void shootingAttack(){...} - all heros from arraylist that can shoot, should shoot!
+     */
+
+}
