55f85ca2e098165ff75cdb7ea8319c62ded4fd86
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-12-22 01:22:00 +0600

    class code

---
+++ src/week9/day4/Tasks.java
@@ -0,0 +1,16 @@
+package week9.day4;
+
+public class Tasks {
+    public static void main(String[] args) {
+
+        /*
+        Do it with Scanner class.
+        Ask the user to enter his birthday: 20.12.1990
+        Then print his age. Your age is 33
+
+        Enter your birthday: 25.12.1990
+        Your age is 32
+         */
+
+    }
+}
