8d44f4d0ace4c4c57d1d6f0f820c3f44459cec2a
Azamat Tolegenov <azicus.sdu@gmail.com>
2023-12-22 02:50:38 +0600

    class code

--- src/week9/day4/Task_3.java
+++ src/week9/day4/Task_3.java
@@ -1,4 +1,42 @@
 package week9.day4;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class Task_3 {
+    public static void main(String[] args) {
+
+        /*
+        Ask the user how many pills he/she have to drink in one day (5)
+        Ask the duration between pills (each 3 hours)
+        Ask the time when he starts to drink pills (08:30)
+
+        1 pill - 08:30
+        2 pill - 11:30
+        3 pill - 14:30
+        4 pill - 17:30
+        5 pill - 20:30
+         */
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("enter how many pills you have: ");
+        int amountOfPills = scan.nextInt();
+
+        System.out.print("enter each how many hours you have drink: ");
+        int hoursPeriod = scan.nextInt();
+
+        System.out.print("enter the time to start drinking pills like (08:00): ");
+        DateTimeFormatter fm1 = DateTimeFormatter.ofPattern("kk:mm");
+        String strTime = scan.next();
+
+        LocalTime time = LocalTime.parse(strTime, fm1);
+
+        for(int i=1; i<=amountOfPills; i++){
+            System.out.println("drink " + i + " pill in " + time.format(fm1));
+            time = time.plusHours(hoursPeriod);
+        }
+
+    }
 }
---
+++ src/week9/day4/Task_4.java
@@ -0,0 +1,16 @@
+package week9.day4;
+
+public class Task_4 {
+    /*
+    Ask the user the amount money he/she has to pay:  60 000 $
+    Ask the user in how many months he/she has to pay: 12
+    Ask when he/she starts to pay: 25.03.2024
+
+    1 pay is in 25.03.2024 - 5000 $
+    2 pay is in 25.04.2024 - 5000 $
+    3 pay is in 25.05.2024 - 5000 $
+    ....
+    11 pay is in 25.01.2025 - 5000 $
+    12 pay is in 25.02.2025 - 5000 $
+     */
+}
